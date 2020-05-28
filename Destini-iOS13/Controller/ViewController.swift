e33a870602ba22590eed1a1914497ff80b6bdb9c
stanmill <stanley.pena29@gmail.com>
2020-05-27 17:26:14 -0700

    story

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateView()
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let userinput = sender.currentTitle!
+        
+        // compares user response and updates the story index array
+        storyBrain.checkUserAnswer(userinput)
+        // Updates the user info
+        updateView()
+    }
+    
+    func updateView() {
+        // get the choices for a specific index in the array of stories
+        let choice1 = storyBrain.getChoice1()
+        let choice2 = storyBrain.getChoice2()
+        storyLabel.text = storyBrain.getQuestions()
+        
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
+        
+    }
+    
 }
 
